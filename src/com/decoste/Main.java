eb7f3bc48fe36f9889e6c0ce5a9447d5a91043ee
Dev DeCoste <DevDeCoste@users.noreply.github.com>
2021-07-28 13:19:17 -0400

    Initial commit

--- src/com/decoste/Main.java
+++ src/com/decoste/Main.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Main {
 
 
-    public static void main(String[] args) {\
+    public static void main(String[] args) {
         /*
         Build an array with all multiples of 3 and 5 within a certain limit, then add all indices in the array together.
          */
