24b4e4436215e703d54f655ff4d1f711f28239e4
Dev DeCoste <DevDeCoste@users.noreply.github.com>
2021-07-30 11:55:04 -0400

    Working on Fibonacci code

--- src/com/decoste/Main.java
+++ src/com/decoste/Main.java
@@ -9,17 +9,18 @@ public class Main {
     public static void main(String[] args) {
         /*
         Build an array with all multiples of 3 and 5 within a certain limit, then add all indices in the array together.
+        https://projecteuler.net/problem=1
          */
+
         ThreeAndFive threeAndFive = new ThreeAndFive();
-
         int m = 1000;
-
         threeAndFive.findSumOfMultiples(m);
+
+        EvenFibonacci evenFib = new EvenFibonacci();
+
 
 
         } // End Main method
-
-
 
     } // End Class Main
 
