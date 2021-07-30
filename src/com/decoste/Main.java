5eab082ffbba1aded08aa14bd96a3ebaf5d640f8
Dev DeCoste <DevDeCoste@users.noreply.github.com>
2021-07-30 10:43:18 -0400

    Moved question 1 logic to its own class to clean up main method and created object inside main.

--- src/com/decoste/Main.java
+++ src/com/decoste/Main.java
@@ -10,48 +10,16 @@ public class Main {
         /*
         Build an array with all multiples of 3 and 5 within a certain limit, then add all indices in the array together.
          */
+        ThreeAndFive threeAndFive = new ThreeAndFive();
 
         int m = 1000;
 
-        findSumOfMultiples(m);
+        threeAndFive.findSumOfMultiples(m);
 
 
         } // End Main method
 
-    static void findSumOfMultiples(int limit) {
-        int count = 0;
-        int size = limit;
-        int multiplesOfThreeOrFive[] = new int[size+1];
 
-        int sumOfMultiples = 0;
-
-
-        for (int x = 1; x < size; x++) {
-            int k = x % 3;
-            int j = x % 5;
-
-            if(k == 0) {
-                //System.out.println(x + " is an multiple of 3");
-                multiplesOfThreeOrFive[count] = x;
-                count++;
-
-            } else if (j == 0) {
-                //System.out.println(x + " is a multiple of 5");
-                multiplesOfThreeOrFive[count] = x;
-                count++;
-            };
-
-        } // End For loop
-
-        System.out.println(Arrays.toString(multiplesOfThreeOrFive));
-
-        for (int p = 0; p < multiplesOfThreeOrFive.length; p++) {
-            sumOfMultiples = sumOfMultiples + multiplesOfThreeOrFive[p];
-
-        }
-        System.out.println("The sum of all multiples of 3 and 5 below " + size + " is " + sumOfMultiples);
-
-    } // End findSumMultiples Method
 
     } // End Class Main
 
--- src/com/decoste/ThreeAndFive.java
+++ src/com/decoste/ThreeAndFive.java
@@ -1,5 +1,44 @@
 package com.decoste;
 
+import java.util.Arrays;
+
 public class ThreeAndFive {
 
-}
+
+    static void findSumOfMultiples(int limit) {
+        int size = limit;
+        int count = 0;
+        int sumOfMultiples = 0;
+        int multiplesOfThreeOrFive[] = new int[size+1];
+
+        for (int x = 1; x < size; x++) {
+            int k = x % 3;
+            int j = x % 5;
+
+            if(k == 0) {
+                //System.out.println(x + " is an multiple of 3");
+                multiplesOfThreeOrFive[count] = x;
+                count++;
+
+            } else if (j == 0) {
+                //System.out.println(x + " is a multiple of 5");
+                multiplesOfThreeOrFive[count] = x;
+                count++;
+            };
+
+        } // End For loop
+
+        System.out.println(Arrays.toString(multiplesOfThreeOrFive));
+
+        for (int p = 0; p < multiplesOfThreeOrFive.length; p++) {
+            sumOfMultiples = sumOfMultiples + multiplesOfThreeOrFive[p];
+
+        }
+        System.out.println("The sum of all multiples of 3 and 5 below " + size + " is " + sumOfMultiples);
+
+    } // End findSumMultiples Method
+
+
+} // End Class
+
+
