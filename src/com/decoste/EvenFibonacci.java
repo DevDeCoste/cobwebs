e38afb47706ef0c38b323e0e10963a4de3e17ce8
Dev <DevDeCoste@users.noreply.github.com>
2021-07-31 16:30:11 -0400

    Fixed fib  method to pull from expected index

--- src/com/decoste/EvenFibonacci.java
+++ src/com/decoste/EvenFibonacci.java
@@ -2,14 +2,14 @@ package com.decoste;
 
 public class EvenFibonacci {
 
-    int n = 5;
+    int n = 7;
 
     static int fib(int n)
     {
         int a = 0, b = 1, c;
         if (n == 0)
             return a;
-        for (int i = 2; i <= n; i++)
+        for (int i = 1; i <= n; i++)
         {
             c = a + b;
             a = b;
@@ -22,8 +22,6 @@ public class EvenFibonacci {
     }
 
     int m = fib(n);
-
-
 
 
 
