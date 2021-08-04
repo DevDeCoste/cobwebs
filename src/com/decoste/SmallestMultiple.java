7a4e9610fe83b8453767d7680d088b7ef8803ae0
Dev DeCoste <DevDeCoste@users.noreply.github.com>
2021-08-04 16:42:37 -0400

    SmallestMultiple method just runs and runs and runs... Working on refactoring my progress using this https://stackoverflow.com/questions/35618229/project-euler-5-in-java-stuck-in-the-end-of-program

--- src/com/decoste/SmallestMultiple.java
+++ src/com/decoste/SmallestMultiple.java
@@ -4,29 +4,37 @@ import java.util.Arrays;
 //2520
 
 public class SmallestMultiple {
-    static int target = 1;
-    static int[] numArray = {1,2,3,4,5,6,7,8,9,10};
+    static long target;
+    static long[] numArray = {1,2,3,4,5,6,7,8,9,10};
 
-    static int smallestMultiple(int[] numArray, int target) {
+    static long smallestMultiple(long[] numArray, long target) {
 
         //These loops are not doing what they should be...
 
-        for(int i = 0; i < numArray.length; i++) {
-            int tempInt;
-            tempInt = numArray[i];
+        for(int i = 21; i > 0; i++) {
+            boolean isDivisible = true;
 
 
-            for(int j = 1; j < 11; j++) {
-                int divisible = tempInt % j;
-                if(divisible != 0) {
+//            long tempInt;
+//            tempInt = numArray[i];
+
+
+            for(int j = 1; j < 20; j++) {
+                int s = i % j;
+//                long divisible = tempInt % j;
+                if(s != 0) {
+                    isDivisible = false;
                     //System.out.print(target + " Is Not a Multiple of " + tempInt + "\n");
-                    target++;
+//                    target++;
 
-                }
+                } else
                 target++;
-                System.out.println(target + " is a multiple of " + tempInt + "\n");
+                //System.out.println(target + " is a multiple of " + tempInt + "\n");
             }
 
+            if(isDivisible) {
+                System.out.println(i);
+            }
 
         }
 
