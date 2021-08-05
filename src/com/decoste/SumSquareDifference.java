2ec8547993d3d773cb26a6a4d242c1fd67bad8f1
Dev DeCoste <DevDeCoste@users.noreply.github.com>
2021-08-05 10:38:08 -0400

    Build Array method updated to include logic to count up to the 10th natural number in the array

--- src/com/decoste/SumSquareDifference.java
+++ src/com/decoste/SumSquareDifference.java
@@ -3,7 +3,7 @@ package com.decoste;
 // Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 public class SumSquareDifference {
-    int arraySize = 100;
+    int arraySize = 10;
     int[] numbers = new int[arraySize];
 
     int sumOfSquares;
@@ -13,10 +13,17 @@ public class SumSquareDifference {
 
     static int[] buildArray(int[] numbers, int arraySize) {
         int count = 1;
-        for(int i = 0; i < arraySize + 1; i++){
+        int sum = 0;
+        for(int i = 0; i <= arraySize - 1; i++){
             numbers[i] = count;
+
+            sum += count;
             count++;
         }
+
+
+
+        System.out.println(sum);
 
         return numbers;
     } //End BuildArray Method
