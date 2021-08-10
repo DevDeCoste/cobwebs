4b78af2604009f38186ff2131d1c3d3b724e69f7
Dev DeCoste <DevDeCoste@users.noreply.github.com>
2021-08-10 14:23:39 -0400

    LPF updated

--- src/com/decoste/LargestPrimeFactor.java
+++ src/com/decoste/LargestPrimeFactor.java
@@ -1,25 +1,34 @@
 package com.decoste;
 
+import java.util.Arrays;
+
 public class LargestPrimeFactor {
     //if target mod 2 equals 1 then it is prime
 
     double target;
-    double[] primeFactors;
+    static double[] primeFactors = new double[10];
     double largestPrime;
+
 // TODO This whole problem
     static void findPrimeFactors(double target) {
         double x = target;
         boolean isPrime = false;
 
+
         // I know this loop isnt complete yet
         for(int i = 2; i < Math.sqrt(x); i++) {
             if(x / 2 != 0) {
+
                 isPrime = true;
+
+                for(int j = 0; j < primeFactors.length; j++) {
+                    primeFactors[j] = x;
+                }
             }
         }
 
         if(isPrime) {
-            System.out.println(x + " is a prime number");
+            System.out.println(Arrays.toString(primeFactors) + " are the prime factors of" + x);
         }
 
 
